d731a8fb0431e039d1489dd101f82f36cab1470d
Nick <niklas.bruhn.dev@gmail.com>
2025-12-28 09:35:09 +0100

    Add presenter

--- backend/connection_manager.py
+++ backend/connection_manager.py
@@ -11,6 +11,8 @@ class ConnectionManager:
         DISCONNECT = "DISCONNECT"
         USER = "USER"
         MESSAGE = "MESSAGE"
+        PRESENTATION = "PRESENTATION"
+        MAIN = "MAIN"
 
     main_sockets: dict[str, WebSocket]
     active_connections: dict[str, list[User]]
@@ -19,18 +21,16 @@ class ConnectionManager:
         self.main_sockets = {}
         self.active_connections = {}
 
-    async def connect(self, websocket: WebSocket, session_id: str, id: str = None, name: str = None) -> User | None:
+    async def connect(self, websocket: WebSocket, session_id: str, user: User = None):
         await websocket.accept()
         if session_id not in self.active_connections:
             self.active_connections[session_id] = []
 
-        if not id:
-            self.main_sockets[session_id] = websocket
-            return None
+        if user:
+            self.active_connections[session_id].append(user)
+            return
+        self.main_sockets[session_id] = websocket
 
-        user = User(id, websocket, session_id, name)
-        self.active_connections[session_id].append(user)
-        return user
 
     def disconnect(self, websocket: WebSocket, session_id: str):
         user = next((u for u in self.active_connections[session_id] if u.is_user(websocket)), None)
@@ -43,17 +43,12 @@ class ConnectionManager:
             for connection in self.active_connections[session_id]:
                 await connection.websocket.send_text(message)
 
-    async def send_to_main(self, user: User, message: str, type: TYPES = TYPES.MESSAGE):
+    async def send_to_main(self, message_obj: dict, session_id: str, type: TYPES = TYPES.MESSAGE):
         try:
-            main_socket = self.main_sockets[user.session_id]
-
-            user_obj = user.as_dict()
-            if message:
-                user_obj["message"] = message
-
+            main_socket = self.main_sockets[session_id]
             await main_socket.send_json({
                 "type": type.value,
-                "data": user_obj
+                "data": message_obj
             })
         except:
-            pass
+            raise
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 from backend.connection_manager import ConnectionManager
+from backend.user import User
 
 app = FastAPI()
 
@@ -39,6 +40,11 @@ async def read_index():
     return FileResponse(FRONTEND_DIR / "questions.html")
 
 
+@app.get("/mobile/{session_id}/presentation")
+async def get_mobile(session_id: str):
+    return FileResponse(FRONTEND_DIR / "presentation.html")
+
+
 @app.get("/mobile/{session_id}")
 async def get_mobile(session_id: str):
     return FileResponse(FRONTEND_DIR / "mobile.html")
@@ -46,39 +52,72 @@ async def get_mobile(session_id: str):
 
 @app.websocket("/ws/{session_id}")
 async def websocket_endpoint(websocket: WebSocket, session_id: str):
-    await websocket_handler(websocket, session_id)
+    await websocket_handler(websocket, session_id, type=ConnectionManager.TYPES.MAIN)
+
+
+@app.websocket("/ws/{session_id}/presentation")
+async def websocket_endpoint(websocket: WebSocket, session_id: str):
+    await websocket_handler(websocket, session_id, type=ConnectionManager.TYPES.PRESENTATION)
 
 
 @app.websocket("/ws/{session_id}/{id}/{name}")
 async def websocket_endpoint(websocket: WebSocket, session_id: str, id: str, name: str):
-    await websocket_handler(websocket, session_id, id, name)
-
-
-async def websocket_handler(websocket: WebSocket, session_id: str, id: str = None, name: str = None):
-    user = await manager.connect(websocket, session_id, id, name)
-    try:
-        if user:
-            await manager.send_to_main(user, "", manager.TYPES.CONNECTION)
-            while True:
-                message = await websocket.receive_text()
-                await manager.send_to_main(user, message)
-        else:
-            await websocket.send_json({
-                "type": manager.TYPES.USER.value,
-                "data": [user.as_dict() for user in manager.active_connections[session_id]],
-            })
-
-            while True:
-                data = await websocket.receive_text()
-                await manager.broadcast(session_id, data)
-    except WebSocketDisconnect:
-        if user:
-            await manager.send_to_main(user, "", manager.TYPES.DISCONNECT)
-        manager.disconnect(websocket, session_id)
-
+    user = User(id=id, websocket=websocket, session_id=session_id, name=name)
+    await websocket_handler(websocket, session_id, user=user)
 
 @app.get("/api/data")
 async def get_data():
     return {
         "url": "http://127.0.0.1:8000",
     }
+
+async def websocket_handler(
+        websocket: WebSocket,
+        session_id: str,
+        user: User = None,
+        type: ConnectionManager.TYPES = None
+):
+    await manager.connect(websocket, session_id, user)
+    try:
+        match type:
+            case ConnectionManager.TYPES.MAIN:
+                await _main_connection(websocket, session_id)
+            case ConnectionManager.TYPES.PRESENTATION:
+                await _presentation_connection(websocket, session_id)
+            case _:
+                await _user_connection(websocket, user, session_id)
+
+        await manager.connect(websocket, session_id, user=user)
+    except WebSocketDisconnect:
+        if type not in [ConnectionManager.TYPES.MAIN, ConnectionManager.TYPES.PRESENTATION]:
+            message_obj = user.as_dict()
+            await manager.send_to_main(message_obj, session_id=session_id, type=manager.TYPES.DISCONNECT)
+        manager.disconnect(websocket, session_id)
+
+
+async def _main_connection(websocket: WebSocket, session_id: str):
+    await websocket.send_json({
+        "type": manager.TYPES.USER.value,
+        "data": [user.as_dict() for user in manager.active_connections[session_id]],
+    })
+
+    while True:
+        data = await websocket.receive_text()
+        await manager.broadcast(session_id, data)
+
+
+async def _presentation_connection(websocket: WebSocket, session_id: str):
+    while True:
+        data = await websocket.receive_text()
+        await manager.send_to_main({"message": data}, session_id=session_id, type=manager.TYPES.PRESENTATION)
+        await manager.broadcast(session_id, data)
+
+
+async def _user_connection(websocket: WebSocket, user: User, session_id: str):
+    await manager.send_to_main(user.as_dict(), session_id=session_id, type=manager.TYPES.CONNECTION)
+    while True:
+        message = await websocket.receive_text()
+        message_obj = user.as_dict()
+        message_obj["message"] = message
+        await manager.send_to_main(message_obj, session_id=session_id)
+
