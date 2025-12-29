11c1892945a94cabb72a11bd47c51f1a58e60e93
Nick <niklas.bruhn.dev@gmail.com>
2025-12-29 09:03:50 +0100

    Add qr code toggle

---
+++ frontend/qr.js
@@ -0,0 +1,14 @@
+const createQRCode = async (selector) => {
+    async function getUrl() {
+        const res = await fetch("api/data");
+        const json = await res.json();
+
+        return json.url
+    }
+
+    const url = await getUrl();
+    const sessionId = window.location.pathname.split('/').pop();
+    const mobileUrl = `${url}/mobile/${sessionId}`;
+
+    new QRCode(selector, mobileUrl);
+}
